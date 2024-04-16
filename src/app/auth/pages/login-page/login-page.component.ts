c07a4737128c0154ee0121fc7d843ea3d5cf9bf7
Abraham Gutierrez <labraham1986@gmail.com>
2024-04-15 23:18:12 -0500

    feat: add routing with lazy loading

---
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
+@Component({
+  templateUrl: './login-page.component.html',
+})
+export class LoginPageComponent {
+
+  private fb          = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router      = inject( Router );
+
+  public myform: FormGroup = this.fb.group({
+    email: ['luis@developer.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]]
+  });
+
+  login() {
+
+    const { email, password } = this.myform.value;
+    this.authService.login(email, password)
+      .subscribe({
+        next: () => this.router.navigateByUrl('/dashboard'),
+        error: ( message ) => {
+          Swal.fire('Error', message, 'error')
+        },
+      });
+  }
+
+}
